68c66b3a81e1c30a6a2c5b30b1e973cdc553b7df
SuperTchain <allurelx@2980.com>
2021-03-21 22:13:19 +0800

    添加redis工具

--- src/main/java/com/lx/povertyalleviation/controller/ShoppingCarController.java
+++ src/main/java/com/lx/povertyalleviation/controller/ShoppingCarController.java
@@ -1,8 +1,8 @@
 package com.lx.povertyalleviation.controller;
 
 import com.lx.povertyalleviation.pojo.Product;
-import com.lx.povertyalleviation.service.ProductService;
 import com.lx.povertyalleviation.service.ShoppingCarService;
+import com.lx.povertyalleviation.utils.RedisUtil;
 import com.lx.povertyalleviation.utils.Result;
 import io.swagger.annotations.Api;
 import org.apache.log4j.Logger;
@@ -10,6 +10,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import redis.clients.jedis.Jedis;
 
 /**
  * @author lx
@@ -31,6 +32,11 @@ public class ShoppingCarController {
     @Autowired
     private ShoppingCarService shoppingCarService;
 
+    private static int ExpireTime = 60*60*24*7;   // redis中存储的过期时间一周
+
+    @Autowired
+    private RedisUtil redisUtil;
+
 
     /**
      * 添加购物车
@@ -40,6 +46,30 @@ public class ShoppingCarController {
     @PostMapping("/addToShoppingCar")
     public Result addToShoppingCar(Product product){
         Result result = new Result();
+        //存放数据
         return result;
+    }
+    @RequestMapping("set")
+    public boolean redisset(String key, String value){
+//        UserEntity userEntity =new UserEntity();
+//        userEntity.setId(Long.valueOf(1));
+//        userEntity.setGuid(String.valueOf(1));
+//        userEntity.setName("zhangsan");
+//        userEntity.setAge(String.valueOf(20));
+//        userEntity.setCreateTime(new Date());
+
+        //return redisUtil.set(key,userEntity,ExpireTime);
+
+        return redisUtil.set(key,value);
+    }
+
+    @RequestMapping("get")
+    public Object redisget(String key){
+        return redisUtil.get(key);
+    }
+
+    @RequestMapping("expire")
+    public boolean expire(String key){
+        return redisUtil.expire(key,ExpireTime);
     }
 }
